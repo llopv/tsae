524a8871b5289283414c46ab4e9529cfa67473cd
David Llop <llop-no-spam-please@protonmail.com>
2017-12-14 16:54:49 +0100

    Phase 1

---
+++ recipes_service/tsae/data_structures/TimestampVector.java
@@ -0,0 +1,148 @@
+/*
+* Copyright (c) Joan-Manuel Marques 2013. All rights reserved.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+*
+* This file is part of the practical assignment of Distributed Systems course.
+*
+* This code is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This code is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this code.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package recipes_service.tsae.data_structures;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+import edu.uoc.dpcs.lsim.LSimFactory;
+import lsim.worker.LSimWorker;
+
+/**
+ * @author Joan-Manuel Marques
+ * December 2012
+ *
+ */
+public class TimestampVector implements Serializable{
+	// Needed for the logging system sgeag@2017
+	private transient LSimWorker lsim = LSimFactory.getWorkerInstance();
+
+	private static final long serialVersionUID = -765026247959198886L;
+	/**
+	 * This class stores a summary of the timestamps seen by a node.
+	 * For each node, stores the timestamp of the last received operation.
+	 */
+	
+	private ConcurrentHashMap<String, Timestamp> timestampVector= new ConcurrentHashMap<String, Timestamp>();
+	
+	public TimestampVector (List<String> participants){
+		// create and empty TimestampVector
+		for (Iterator<String> it = participants.iterator(); it.hasNext(); ){
+			String id = it.next();
+			// when sequence number of timestamp < 0 it means that the timestamp is the null timestamp
+			timestampVector.put(id, new Timestamp(id, Timestamp.NULL_TIMESTAMP_SEQ_NUMBER));
+		}
+	}
+
+	/**
+	 * Updates the timestamp vector with a new timestamp. 
+	 * @param timestamp
+	 */
+	public void updateTimestamp(Timestamp timestamp){
+		timestampVector.put(timestamp.getHostid(), timestamp);
+	}
+	
+	/**
+	 * merge in another vector, taking the elementwise maximum
+	 * @param tsVector (a timestamp vector)
+	 */
+	public void updateMax(TimestampVector tsVector){
+		for (Iterator<String> it = tsVector.timestampVector.keySet().iterator(); it.hasNext(); ){
+			String k = it.next();
+			Timestamp t1 = tsVector.timestampVector.get(k);
+			Timestamp t2 = timestampVector.get(k);
+			updateTimestamp(t1.compare(t2) > 0 ? t1 : t2);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param node
+	 * @return the last timestamp issued by node that has been
+	 * received.
+	 */
+	public Timestamp getLast(String node){
+		return timestampVector.get(node);
+	}
+	
+	/**
+	 * merges local timestamp vector with tsVector timestamp vector taking
+	 * the smallest timestamp for each node.
+	 * After merging, local node will have the smallest timestamp for each node.
+	 *  @param tsVector (timestamp vector)
+	 */
+	public void mergeMin(TimestampVector tsVector){
+		for (Iterator<String> it = tsVector.timestampVector.keySet().iterator(); it.hasNext(); ){
+			String k = it.next();
+			Timestamp t1 = tsVector.timestampVector.get(k);
+			Timestamp t2 = timestampVector.get(k);
+			updateTimestamp(t1.compare(t2) < 0 ? t1 : t2);
+		}
+	}
+	
+	/**
+	 * clone
+	 */
+	public TimestampVector clone(){
+		TimestampVector t = new TimestampVector(new ArrayList<String>());
+		for (Iterator<String> it = timestampVector.keySet().iterator(); it.hasNext(); ){
+			String id = it.next();
+			// when sequence number of timestamp < 0 it means that the timestamp is the null timestamp
+			t.timestampVector.put(id, timestampVector.get(id));
+		}
+		return t;
+	}
+	
+	/**
+	 * equals
+	 */
+	public boolean equals(Object obj){
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TimestampVector other = (TimestampVector) obj;
+		return other.timestampVector.equals(timestampVector);
+	}
+
+	/**
+	 * toString
+	 */
+	@Override
+	public synchronized String toString() {
+		String all="";
+		if(timestampVector==null){
+			return all;
+		}
+		for(Enumeration<String> en=timestampVector.keys(); en.hasMoreElements();){
+			String name=en.nextElement();
+			if(timestampVector.get(name)!=null)
+				all+=timestampVector.get(name)+"\n";
+		}
+		return all;
+	}
+}
---
+++ recipes_service/tsae/sessions/TSAESessionPartnerSide.java
@@ -0,0 +1,128 @@
+/*
+* Copyright (c) Joan-Manuel Marques 2013. All rights reserved.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+*
+* This file is part of the practical assignment of Distributed Systems course.
+*
+* This code is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This code is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this code.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package recipes_service.tsae.sessions;
+
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+import communication.ObjectInputStream_DS;
+import communication.ObjectOutputStream_DS;
+import recipes_service.ServerData;
+import recipes_service.communication.Message;
+import recipes_service.communication.MessageAErequest;
+import recipes_service.communication.MessageEndTSAE;
+import recipes_service.communication.MessageOperation;
+import recipes_service.communication.MsgType;
+import recipes_service.data.Operation;
+import recipes_service.tsae.data_structures.TimestampMatrix;
+import recipes_service.tsae.data_structures.TimestampVector;
+
+//LSim logging system imports sgeag@2017
+import lsim.worker.LSimWorker;
+import edu.uoc.dpcs.lsim.LSimFactory;
+import edu.uoc.dpcs.lsim.logger.LoggerManager.Level;
+
+/**
+ * @author Joan-Manuel Marques
+ * December 2012
+ *
+ */
+public class TSAESessionPartnerSide extends Thread{
+	// Needed for the logging system sgeag@2017
+	private LSimWorker lsim = LSimFactory.getWorkerInstance();
+	
+	private Socket socket = null;
+	private ServerData serverData = null;
+	
+	public TSAESessionPartnerSide(Socket socket, ServerData serverData) {
+		super("TSAEPartnerSideThread");
+		this.socket = socket;
+		this.serverData = serverData;
+	}
+
+	public void run() {
+
+		Message msg = null;
+
+		int current_session_number = -1;
+		try {
+			ObjectOutputStream_DS out = new ObjectOutputStream_DS(socket.getOutputStream());
+			ObjectInputStream_DS in = new ObjectInputStream_DS(socket.getInputStream());
+			
+			TimestampVector localSummary = this.serverData.getSummary().clone();
+			TimestampMatrix localAck = this.serverData.getAck().clone();
+
+			// receive originator's summary and ack
+			msg = (Message) in.readObject();
+			current_session_number = msg.getSessionNumber();
+			lsim.log(Level.TRACE, "[TSAESessionPartnerSide] [session: "+current_session_number+"] TSAE session");
+			lsim.log(Level.TRACE, "[TSAESessionPartnerSide] [session: "+current_session_number+"] received message: "+ msg);
+			if (msg.type() == MsgType.AE_REQUEST){
+				// ...
+				
+	            // send operations
+					// ...
+					msg.setSessionNumber(current_session_number);
+					out.writeObject(msg);
+					lsim.log(Level.TRACE, "[TSAESessionPartnerSide] [session: "+current_session_number+"] sent message: "+ msg);
+
+				// send to originator: local's summary and ack
+				// ...
+				msg = new MessageAErequest(localSummary, localAck);
+				msg.setSessionNumber(current_session_number);
+	 	        out.writeObject(msg);
+				lsim.log(Level.TRACE, "[TSAESessionPartnerSide] [session: "+current_session_number+"] sent message: "+ msg);
+
+	            // receive operations
+				msg = (Message) in.readObject();
+				lsim.log(Level.TRACE, "[TSAESessionPartnerSide] [session: "+current_session_number+"] received message: "+ msg);
+				while (msg.type() == MsgType.OPERATION){
+					// ...
+					msg = (Message) in.readObject();
+					lsim.log(Level.TRACE, "[TSAESessionPartnerSide] [session: "+current_session_number+"] received message: "+ msg);
+				}
+				
+				// receive message to inform about the ending of the TSAE session
+				if (msg.type() == MsgType.END_TSAE){
+					// send and "end of TSAE session" message
+					msg = new MessageEndTSAE();
+					msg.setSessionNumber(current_session_number);
+		            out.writeObject(msg);					
+					lsim.log(Level.TRACE, "[TSAESessionPartnerSide] [session: "+current_session_number+"] sent message: "+ msg);
+				}
+				
+			}
+			socket.close();		
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			lsim.log(Level.FATAL, "[TSAESessionPartnerSide] [session: "+current_session_number+"]" + e.getMessage());
+			e.printStackTrace();
+            System.exit(1);
+		}catch (IOException e) {
+	    }
+		
+		lsim.log(Level.TRACE, "[TSAESessionPartnerSide] [session: "+current_session_number+"] End TSAE session");
+	}
+}
